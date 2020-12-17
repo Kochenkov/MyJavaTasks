e0df792e311ca4148b657aa5f1d509cb90e9c8c6
Vlad Kochenkov <kochenkov1993@gmail.com>
2020-12-17 15:56:55 +0300

    added yandex task sprint 2

---
+++ src/main/java/from_yandex_praktikum_algorithms/sprint_2/final_tasks/CalculatorTask.java
@@ -0,0 +1,108 @@
+package from_yandex_praktikum_algorithms.sprint_2.final_tasks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Посылка: 45596475
+ Выполнил: Коченков Владислав
+ *
+ * -- ПРИНЦИП РАБОТЫ --
+ * Решил реализовать эту задачу на связном списке, а не на стэке. Тк стэк, все-равно использовал бы связный список в своей реализации.
+ * Принцип работы алгоритма заключается в том, что я считываю сразу всю строку и храню ее в связном списке.
+ * Далее, пока в списке есть хотя бы один оператор, выполняю функцию выполнения операции.
+ * Для этого, иду по списку, до первого встречного оператора, и выполняю операцию с этим оператором, для двух предыдущих элементов.
+ *
+ * -- КОРРЕКТНОСТЬ РАБОТЫ --
+ * Само условие польской нотации гарантирует, что перед оператором будет минимум два операнда, значит алгоритм будет работать корректно.
+ * Столкнулся со сложностью при реализации деления, долго не мог понять, как именно оно должно работать.
+ *
+ * -- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+ * Сам алгоритм в целом, выполняется за О(n), тк количество итераций напрямую зависит от размера входных данных
+ * Функция обработки конкретного участка списка с двумя операндами и оператором работает за O(1), тк при реализации используется связный список,
+ * и не приходится перезаписывать весь массив оставшихся данных целиком.
+ */
+public class CalculatorTask {
+
+    static List<String> inputList;
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        inputList = new LinkedList<>(Arrays.asList(in.readLine().split(" ")));
+        in.close();
+
+        while (inputList.get(inputList.size() - 1).equals("/") ||
+                inputList.get(inputList.size() - 1).equals("*") ||
+                inputList.get(inputList.size() - 1).equals("+") ||
+                inputList.get(inputList.size() - 1).equals("-")
+        ) {
+            inputList = findAndDoOperation(inputList);
+        }
+
+        String answer;
+        if (inputList.size() > 1) {
+            answer = inputList.get(inputList.size() - 1);
+        } else {
+            answer = inputList.get(0);
+        }
+        System.out.println(answer);
+    }
+
+    private static List<String> findAndDoOperation(List<String> inputList) {
+        String firstItem = inputList.get(0);
+        String secondItem = inputList.get(1);
+        for (int i = 2; i < inputList.size(); i++) {
+            if (inputList.get(i).equals("*")) {
+                Integer a = Integer.parseInt(firstItem);
+                Integer b = Integer.parseInt(secondItem);
+                Integer c = a * b;
+                replace(i, c.toString());
+                break;
+            } else if (inputList.get(i).equals("/")) {
+                Integer a = Integer.parseInt(firstItem);
+                Integer b = Integer.parseInt(secondItem);
+                Integer c;
+                if (b != 0) {
+                    c = a / b;
+                    if (c == 0) {
+                        if (a < 0 || b < 0) {
+                            c = -1;
+                        } else {
+                            c = a % b;
+                        }
+                    }
+                } else {
+                    throw new IllegalArgumentException();
+                }
+                replace(i, c.toString());
+                break;
+            } else if (inputList.get(i).equals("+")) {
+                Integer a = Integer.parseInt(firstItem);
+                Integer b = Integer.parseInt(secondItem);
+                Integer c = a + b;
+                replace(i, c.toString());
+                break;
+            } else if (inputList.get(i).equals("-")) {
+                Integer a = Integer.parseInt(firstItem);
+                Integer b = Integer.parseInt(secondItem);
+                Integer c = a - b;
+                replace(i, c.toString());
+                break;
+            } else {
+                firstItem = inputList.get(i - 1);
+                secondItem = inputList.get(i);
+            }
+        }
+        return inputList;
+    }
+
+    private static void replace(int index, String item) {
+        inputList.set(index, item);
+        inputList.remove(index - 2);
+        inputList.remove(index - 2);
+    }
+}
--- src/main/java/from_yandex_praktikum_algorithms/sprint_2/simple_tasks/A.java
+++ src/main/java/from_yandex_praktikum_algorithms/sprint_2/simple_tasks/A.java
@@ -1,6 +1,8 @@
 package from_yandex_praktikum_algorithms.sprint_2.simple_tasks;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -8,23 +10,25 @@ import java.io.InputStreamReader;
 public class A {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        short lines = Short.parseShort(in.readLine()
-                                         .split(" ")[0]);
-        short columns = Short.parseShort(in.readLine()
-                                           .split(" ")[0]);
-        short[][] matrix = new short[lines][columns];
+        final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter out = new BufferedWriter(new FileWriter("output.txt"));
+
+        final short lines = Short.parseShort(in.readLine()
+                                               .split(" ")[0]);
+        final short columns = Short.parseShort(in.readLine()
+                                                 .split(" ")[0]);
+        String[][] matrix = new String[lines][];
         for (short i = 0; i < lines; i++) {
-            String[] inStr = in.readLine()
-                               .split(" ");
-            for (short j = 0; j < columns; j++) {
-                matrix[j][i] = Short.parseShort(inStr[j]);
-                //log
-                System.out.print(matrix[j][i] + " ");
-            }
-            System.out.println();
+            matrix[i] = in.readLine().split(" ");
         }
         in.close();
 
+        for (short i = 0; i < columns; i++) {
+            for (short j = 0; j < lines; j++) {
+                out.write(matrix[j][i] + " ");
+            }
+            out.newLine();
+        }
+        out.close();
     }
 }
